004d9a3747552bfcfb22cefa825419d0acceb0a9
jayjshah <jayjshah.212@gmail.com>
2021-06-16 22:06:08 +0530

    spawn

--- subd.py
+++ subd.py
@@ -5,11 +5,7 @@ Created on Mon Feb  8 11:45:23 2021
 @author: Annonymous
 """
 
-import logging as log
-from msilib.schema import Font
-from tkinter import font
-from turtle import width
-from pyfiglet import Figlet
+
 import pyfiglet
 import requests
 import time
